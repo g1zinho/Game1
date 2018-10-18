00c52fb25da8045f3c605506f1d3cd5bd1855a56
Guilherme Brasilino <ge.um@hotmail.com>
2018-10-18 08:53:06 -0300

    contador na barra de vida

--- src/com/gibiris/graficos/UI.java
+++ src/com/gibiris/graficos/UI.java
@@ -1,6 +1,7 @@
 package com.gibiris.graficos;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import com.gibirus.entities.Player;
@@ -12,6 +13,9 @@ public class UI {
 		g.fillRect(10, 4, 50, 8);
 		g.setColor(Color.green);
 		g.fillRect(10, 4,(int)((Player.life/Player.maxlife)*50), 8);
+		g.setColor(Color.black);
+		g.setFont(new Font("arial", Font.BOLD, 9));
+		g.drawString((int) Player.life+"/"+(int)Player.maxlife, 10, 12);
 	}
 
 }
